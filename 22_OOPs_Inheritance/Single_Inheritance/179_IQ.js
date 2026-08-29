f27e9b37a5d645d98ddd91da868985ae7f32a939
Kavita Batra <kavitabatra413@gmail.com>
2026-08-29 21:10:53 +0530

    Add learning notes and code exercises

---
+++ 22_OOPs_Inheritance/Single_Inheritance/179_IQ.js
@@ -0,0 +1,31 @@
+class BaseTest {
+    constructor(){
+        console.log("Parent!")
+    }
+    setup() {
+        console.log("Base: open browser");
+    }
+    teardown() {
+        console.log("Base: close browser");
+    }
+}
+class UITest extends BaseTest {
+    constructor(){
+        super();
+    }
+    setup() {
+        super.setup(); // UITest will help you to call your parent function.
+        // super() - Constrcutor, super.fname() - functions name
+        console.log("UI: maximize window");
+    }
+
+    teardown() {
+        console.log("UI: take screenshot");
+        super.teardown();
+    }
+
+}
+
+let test = new UITest();
+test.setup();
+test.teardown();
